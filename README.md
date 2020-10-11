9bc01c65798b24baf808123ecf3ba632bc48ca66
amandotzip <aawoke@terpmail.umd.edu>
2020-10-11 02:34:24 -0400

    encrypting and decrypting essentailly work! Readme also clearer

--- README.md
+++ README.md
@@ -1,5 +1,46 @@
-# lockjaw
-Encrypts archives behind face recognition
+# Lockjaw
+Encrypts data into archives via facial recognition
+
+# Overview
+This program uses webcam video feed to register your face in its database, allowing you to encrypt files into an archive. You may use you face as a key to then decrypt your archives.
+
+# Register
+Register your face with option 1, your face will be scanned, a password will be generated alongside the scan, and you may encrypt files.
+
+List out files you'd like to archive and encrypt For example
+
+```
+Choose files to encrypt:
+images\potter.jpg images\granger.jpg images\weasley.jpg
+```
+Choose a name for the archive
+```
+Choose a snazzy name for the archive: 
+yearbook
+```
+
+It is now stored in an encrypted archive.
+
+# Accessing encrypted archives/decrypting
+Select option 2 to decrypt archives]
+Your face will now be scanned, and be matched with the database of stored face encodings. 
+
+Once a registered user has their face scanned, they will be welcomed and may select to decrypt their files.
+
+
+```
+Scanning for your gorgeous face...
+Welcome Master Dumbledore. What would you like to decrypt?
+yearbook.zip
+Uncompressing...
+Uncompressing and decryption successful!
+```
+
+If you see the above success text, you should see the archive contents unzipped.
+# Design
+All face encodings, usernames, and passwords are stored in pickle files. Pickle files are able to store objects for use in another run of the program.
+
+clear_database: empties face encodings, username, and password pickle files.
 
 # Docs referenced
 https://pypi.org/project/face-recognition/ 
@@ -7,10 +48,3 @@ https://github.com/ageitgey/face_recognition/blob/master/examples/facerec_from_w
 https://py7zr.readthedocs.io/en/latest/api.html
 
 minizip used to encrypt zip folders with compress and uncompress
-
-faces saved in known_face_encoding
-
-register, save your face encoding
-password generated from face encoding
-you lock each file with teh same password associated with that face
-
--- face.py
+++ face.py
@@ -1,3 +1,17 @@
+def clear_database():
+    with open('face_encodings.pickle', 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump([], f, pickle.HIGHEST_PROTOCOL)
+
+    with open('face_names.pickle', 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump([], f, pickle.HIGHEST_PROTOCOL)
+    
+    with open('passwords.pickle', 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump([], f, pickle.HIGHEST_PROTOCOL)
+
+
 import face_recognition
 import cv2
 import numpy as np
@@ -5,9 +19,7 @@ import py7zr
 import pyminizip
 import secrets
 import string
-
-
-
+import pickle
 
 
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
@@ -15,67 +27,30 @@ import string
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
 #   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+print("   ///////\n  LOCKJAW \n ///////")
 
 # Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-# Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("donald.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("savage.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
-
-known_face_names = [
-    "Donald Glover",
-    "21 Savage"
-]
-
-
-#take username and password text files and read them in for program
-# define empty list
-users = []
-
-# open file and read the content in a list
-with open('usernames.txt', 'r') as filehandle:
-    filecontents = filehandle.readlines()
-
-    for line in filecontents:
-        # remove linebreak which is the last character of the string
-        current_user = line[:-1]
-        # add item to the list
-        users.append(current_user)
-
-
-
+known_face_encodings = []
+known_face_names = []
 passwords = []
-# open file and read the content in a list
-with open('passwords.txt', 'r') as filehandle:
-    filecontents = filehandle.readlines()
 
-    for line in filecontents:
-        # remove linebreak which is the last character of the string
-        current_pass = line[:-1]
-        # add item to the list
-        passwords.append(current_pass)
+with open('face_encodings.pickle', 'rb') as f:
+    known_face_encodings = pickle.load(f)
 
+with open('face_names.pickle', 'rb') as f:
+    known_face_names = pickle.load(f)
+
+with open('passwords.pickle', 'rb') as f:
+    passwords = pickle.load(f)
 
 
 #Choose option to register your face or open encrypted files
-option = input("1 to register, 2 decrypt archives")
+option = input("Select an option below:\nRegister: \t\t1\nDecrypt archives: \t2\n")
 
 if option == "1":
-    username = input("Please enter a username: ")
+    username = input("Please enter a username\n")
     # Initialize some variables
     face_locations = []
     face_encodings = [] 
@@ -91,7 +66,7 @@ if option == "1":
         ret, frame = video_capture.read()
 
         # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        small_frame = cv2.resize(frame, (0, 0), fx=.25, fy=.25)
 
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame[:, :, ::-1]
@@ -101,12 +76,6 @@ if option == "1":
             # Find all the faces and face encodings in the current frame of video
             face_locations = face_recognition.face_locations(rgb_small_frame)
             face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-            
-
-
-            
-           
-
 
             face_names = []
             for face_encoding in face_encodings:
@@ -115,17 +84,22 @@ if option == "1":
                
                 
                 #add to registered names and encodings 
+                #append to lists, then update the pickle file
                 known_face_encodings.append(face_encoding)
                 known_face_names.append(username)
 
-                #A face was found so add name into system, then decalre registerd to stop looking at video
-                users.append(username)
-                print(users)
-                with open('usernames.txt', 'w') as filehandle:
-                    for listitem in users:
-                        filehandle.write('%s\n' % listitem)
-                registered = True
+                with open('face_encodings.pickle', 'wb') as f:
+                    # Pickle the 'data' dictionary using the highest protocol available.
+                    pickle.dump(known_face_encodings, f, pickle.HIGHEST_PROTOCOL)
 
+                with open('face_names.pickle', 'wb') as f:
+                    # Pickle the 'data' dictionary using the highest protocol available.
+                    pickle.dump(known_face_names, f, pickle.HIGHEST_PROTOCOL)
+
+
+                #A face was found so add name into system, 
+                # then declare registerd to stop looking at video
+                registered = True
 
 
                 # See if the face is a match for the known face(s)
@@ -175,25 +149,125 @@ if option == "1":
     video_capture.release()
     cv2.destroyAllWindows()
 
+    
+       
+    print("Registration successful!")
+    # going to have to make this more friendly than literall wrtiing a list
+    src_files = input("Welcome " + username + ". Choose files to encrypt:\n").split(" ")
+    archive_name = input("Choose a snazzy name for the archive:\n")
 
-alphabet = string.ascii_letters + string.digits
-password = ''.join(secrets.choice(alphabet) for i in range(20)) 
-passwords.append(password)
-with open('passwords.txt', 'w') as filehandle:
-    for listitem in passwords:
-        filehandle.write('%s\n' % listitem)
+    src_paths = []
+
+    for files in src_files:
+        src_paths.append(archive_name)
+    
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(alphabet) for i in range(2000))
+    print(password)
+    passwords.append(password)
+    with open('passwords.pickle', 'wb') as f:
+        # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump(passwords, f, pickle.HIGHEST_PROTOCOL) 
+
+    print("Compressing...")
+    #minizip encryption
+    compression_level = 5 # 1-9
+    pyminizip.compress_multiple(src_files, src_paths, ".\\" + archive_name + ".zip", password, compression_level)
+
+    print("Compressing and encryption successful!")
 
 
 
-# going to have to make this more friendly than literall wrtiing a list
-src_files = input("Welcome " + username + ". Choose files to encrypt: ").split(" ")
-archive_name = input("Choose a snazzy name for the archive: ")
+elif option == "2":
+    # Initialize some variables
+    face_locations = []
+    face_encodings = [] 
+    face_names = []
+    process_this_frame = True
 
-src_paths = []
+    current_person = ""
+    current_password = ""
+    userfound = False
+    print("Scanning for your gorgeous face...")
 
-for files in src_files:
-    src_paths.append(archive_name)
+    #As soon as the first face is found, you are registered and we will stop scanning video feed
+    while not userfound:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
 
-#minizip encryption
-compression_level = 5 # 1-9
-pyminizip.compress_multiple(src_files, src_paths, ".\\" + archive_name + ".zip", password, compression_level)
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            
+            #face_encodings = all faces in current frame
+            face_names = []
+            for face_encoding in face_encodings:
+                
+                
+                #will take the first face it sees, can restrict it to only allow 1 later
+                #store in file of registered users
+
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+
+                # # If a match was found in known_face_encodings, just use the first one.
+                # if True in matches:
+                #     first_match_index = matches.index(True)
+                #     name = known_face_names[first_match_index]
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+                    current_person = name
+
+                    current_password = passwords[best_match_index]
+                    userfound = True
+
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+    target = input("Welcome Master " + current_person +". What would you like to decrypt?\n")
+    print("Uncompressing...")
+    pyminizip.uncompress(target, current_password, None, 0)
+    print("Uncompressing and decryption successful!")
